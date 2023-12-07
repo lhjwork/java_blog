feec7ece877113b2e7488b9728674760ecb0a41e
lhjwork <jin.come.up.business@gmail.com>
2023-12-07 12:37:48 +0900

    OneToMany Fetch전략 EAGER and Lazy 일경우 구분  reply List<Reply> 적용

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,18 +1,21 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -42,9 +45,14 @@ public class Board {
 	private int count; // 조회수
 	
 	
-	@ManyToOne //board가 many user가 one에 해당한다. (Many = board, one = user) 한명의 user는 여러개의 게시글을 쓸 수 있다는 의미
+	@ManyToOne(fetch=FetchType.EAGER) //board가 many user가 one에 해당한다. (Many = board, one = user) 한명의 user는 여러개의 게시글을 쓸 수 있다는 의미
 	@JoinColumn(name="userId")
 	private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다. 
+	
+	
+	@OneToMany(mappedBy="board", fetch=FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다. (난 FK가 아니에요.) DB에 컬럼을 만들지 마세요.
+	private List<Reply> reply;
+	
 	
 //	sql로 했을 경우에는
 //	private int userId; // forienKey를 줘야한다. 
