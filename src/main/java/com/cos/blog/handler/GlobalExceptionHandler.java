450d195de92c384613e68d580fae9b5a78c7614a
lhjwork <jin.come.up.business@gmail.com>
2023-12-10 16:27:31 +0900

    username unique setting and main page create and bootstrap

--- src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GlobalExceptionHandler {
 
+	//IllegalArgumentException 만 받음
 	@ExceptionHandler(value=IllegalArgumentException.class)
 	public String handleArgumentException(IllegalArgumentException e) {
 		return "<h1>"+ e.getMessage() + "</h1>";
 	}
 	
-	
+	//Exception -> 모든 Exception을 받음
 	@ExceptionHandler(value=Exception.class)
 	public String handleException(IllegalArgumentException e) {
 		return "<h1>"+ e.getMessage() + "</h1>";
