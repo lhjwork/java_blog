61b268676aa25e138f3dac68ea1f6936a1d267a5
lhjwork <jin.come.up.business@gmail.com>
2023-12-10 22:24:22 +0900

    Controller(index method) : UserController -> BoardController

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -6,11 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 	
-	//아무것도 안적을 때와 / 만 했을 떄 저리로 감
-	@GetMapping({"","/"})
-	public String index() {
-		
-		// /WEB-INF/views/index.jsp
-		return "index";
-	}
+
 }
