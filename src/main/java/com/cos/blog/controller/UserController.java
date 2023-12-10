f9d0df86081d49303186625ce8d03d211ee1a72e
lhjwork <jin.come.up.business@gmail.com>
2023-12-10 23:00:46 +0900

    login and join(sign-up_) page and GetMapping

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -6,5 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 	
-
+	@GetMapping("/user/joinForm")
+	public String joinForm() {
+		
+		return "/user/joinForm";
+	}
+	
+	@GetMapping("/user/loginForm")
+	public String loginForm() {
+		
+		return "/user/loginForm";
+	}
+	
 }
