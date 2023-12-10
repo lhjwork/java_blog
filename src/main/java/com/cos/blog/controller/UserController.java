fda52436f116ab242a693090badb74265f12c715
lhjwork <jin.come.up.business@gmail.com>
2023-12-10 16:58:59 +0900

    index.jsp : /blog page UI 수정

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 	
-	@GetMapping("/")
+	//아무것도 안적을 때와 / 만 했을 떄 저리로 감
+	@GetMapping({"","/"})
 	public String index() {
 		
 		// /WEB-INF/views/index.jsp
