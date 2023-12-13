a736f55199687032a0e1e896573865dae43554b4
lhjwork <jin.come.up.business@gmail.com>
2023-12-13 14:03:31 +0900

    시큐리티 시작전 요청 주소 변경

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -25,13 +25,13 @@ let index = {
 		$.ajax({
 			type:"POST",
 			//url:"/blog/api/user",
-			url:"/blog/api/user/login",
+			url:"/api/user/login",
 			data : JSON.stringify(data), // http body 데이터
 			contentType: 'application/json', // body데이터가 어떤 타입입지(MIME)
 			dataType:"json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이러면) => javascript 오브젝트로 변경
 		}).done(function(resp){
 			alert("로그인이 완료되었습니다.");
-			location.href = "/blog";
+			location.href = "/";
 		}).fail(function(err){
 			alert(JSON.stringify(err));
 		}); 
@@ -55,13 +55,13 @@ let index = {
 		$.ajax({
 			type:"POST",
 			//url:"/blog/api/user",
-			url:"/blog/api/user",
+			url:"/api/user",
 			data : JSON.stringify(data), // http body 데이터
 			contentType: 'application/json', // body데이터가 어떤 타입입지(MIME)
 			dataType:"json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이러면) => javascript 오브젝트로 변경
 		}).done(function(resp){
 			alert("회원가입이 완료되었습니다.");
-			location.href = "/blog";
+			location.href = "/";
 		}).fail(function(err){
 			alert(JSON.stringify(err));
 		}); 
